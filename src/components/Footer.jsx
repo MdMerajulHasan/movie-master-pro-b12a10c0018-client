bbce3e5ea5106fbac024ef01ac69a9737947fb7c
Jim <merajuljim@gmai.com>
2025-11-12 22:03:48 +0600

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router";
+
+const Footer = () => {
+  return (
+    <div className="bg-black py-5 md:py-10 text-white text-xs font-bold">
+      <div className="flex flex-col justify-center items-center md:flex-row md:justify-around md:items-start">
+        <div>
+          <h5 className="text-base">Go to</h5>
+          <ul className="space-y-1">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/movies">All Movies</Link>
+            </li>
+            <li>
+              <Link to="/movies/my-collection">My Collection</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h5 className="text-base">Find Us On</h5>
+          <div className="flex flex-col gap-1 md:flex-row md:gap-2 md:items-center">
+            <FaFacebook size={20}></FaFacebook>
+            <FaLinkedin size={20}></FaLinkedin>
+            <FaGithub size={20}></FaGithub>
+          </div>
+        </div>
+      </div>
+      <p className="text-center"> Copyright &copy; 2025 Movie Master Pro </p>
+    </div>
+  );
+};
+
+export default Footer;
